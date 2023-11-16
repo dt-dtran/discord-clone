f398cdcaeec223bce8cc86d79e8c4fff620eee49
Diana <75264344+dt-dtran@users.noreply.github.com>
2023-11-15 21:57:30 -0700

    delete server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,7 +7,7 @@ import { InviteModal } from "@/components/modals/invite-modal";
 import { MembersModal } from "@/components/modals/members-modal";
 import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
-// import { DeleteServerModal } from "@/components/modals/delete-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 // import { DeleteChannelModal } from "@/components/modals/delete-channel-modal";
 // import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 // import { MessageFileModal } from "@/components/modals/message-file-modal";
@@ -31,9 +31,9 @@ export const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
-      <LeaveServerModal />{" "}
-      {/*
+      <LeaveServerModal />
       <DeleteServerModal />
+      {/*
       <DeleteChannelModal />
       <EditChannelModal />
       <MessageFileModal />
