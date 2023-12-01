fd796225ee37b500a2645feffe4da1a266ade2b7
Diana <75264344+dt-dtran@users.noreply.github.com>
2023-11-30 23:05:04 -0700

    added edit feature to channels

--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -3,7 +3,7 @@ import * as z from "zod";
 import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 import { ChannelType } from "@prisma/client";
 import { useEffect } from "react";
@@ -45,40 +45,40 @@ const formSchema = z.object({
   type: z.nativeEnum(ChannelType),
 });
 
-export const CreateChannelModal = () => {
+export const EditChannelModal = () => {
   const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
-  const params = useParams();
 
-  const isModalOpen = isOpen && type == "createChannel";
+  const isModalOpen = isOpen && type == "editChannel";
 
-  const { channelType } = data;
+  const { channel, server } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: channelType || ChannelType.TEXT,
+      type: channel?.type || ChannelType.TEXT,
     },
   });
 
   useEffect(() => {
-    if (channelType) {
-      form.setValue("type", channelType);
-    } else [form.setValue("type", ChannelType.TEXT)];
-  }, [channelType, form]);
+    if (channel) {
+      form.setValue("name", channel.name);
+      form.setValue("type", channel.type);
+    }
+  }, [form, channel]);
 
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = qs.stringifyUrl({
-        url: "/api/channels",
+        url: `/api/channels/${channel?.id}`,
         query: {
-          serverId: params?.serverId,
+          serverId: server?.id,
         },
       });
-      await axios.post(url, values);
+      await axios.patch(url, values);
       console.log("values", values);
       form.reset();
       router.refresh();
@@ -99,7 +99,7 @@ export const CreateChannelModal = () => {
         <DialogContent className="bg-white text-black p-0 overflow-hidden">
           <DialogHeader className="pt-8 px-6">
             <DialogTitle className="text-center text-2xl font-bold">
-              Create channel
+              Edit channel
             </DialogTitle>
           </DialogHeader>
           <Form {...form}>
@@ -160,7 +160,7 @@ export const CreateChannelModal = () => {
               </div>
               <DialogFooter className="bg-gray-100 px-6 py-4">
                 <Button disabled={isLoading} variant="primary">
-                  Create
+                  Save
                 </Button>
               </DialogFooter>
             </form>
