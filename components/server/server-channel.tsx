ca7b13d62878fa5bc3d5db918a22f881dd23df3e
Diana <75264344+dt-dtran@users.noreply.github.com>
2023-12-07 18:53:18 -0700

    added profile to bottom of sidebar

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -74,7 +74,7 @@ export const ServerChannel = ({
         </div>
       )}
       {channel.name === "general" && (
-        <Lock className="ml-auto w-4 h04 text-zinc-500 dark:text-zinc-400" />
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
       )}
     </button>
   );
