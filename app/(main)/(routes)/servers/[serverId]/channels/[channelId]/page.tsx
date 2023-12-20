3a1a072d117c9a3bd7b3639d7ee4fee182970fb5
Diana <75264344+dt-dtran@users.noreply.github.com>
2023-12-20 15:44:07 -0700

    chat welcome header for text channel

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -4,6 +4,8 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-message";
+
 interface ChannelIdPageProps {
   params: {
     serverId: string;
@@ -36,21 +38,30 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   }
 
   return (
-    <div>
-      <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
-        <ChatHeader
-          name={channel.name}
-          serverId={channel.serverId}
-          type="channel"
-        />
-        <div className="flex-1">Future Messages</div>
-        <ChatInput
-          name={channel.name}
-          type="channel"
-          apiUrl="/api/socket/messages"
-          query={{ channelId: channel.id, serverId: channel.serverId }}
-        />
-      </div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={channel.serverId}
+        type="channel"
+      />
+
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        chatId={channel.id}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{ channelId: channel.id, serverId: channel.serverId }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ channelId: channel.id, serverId: channel.serverId }}
+      />
     </div>
   );
 };
