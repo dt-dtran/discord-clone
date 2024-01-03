ae10dd72e1363f806ac455e88fdeeb9d75d4ec45
Diana <75264344+dt-dtran@users.noreply.github.com>
2024-01-03 04:04:52 -0700

    updated env

--- pages/api/socket/io.ts
+++ pages/api/socket/io.ts
@@ -16,6 +16,15 @@ const ioHandler = (req: NextApiRequest, res: NextApiResponseServerIo) => {
     const io = new ServerIO(httpServer, {
       path: path,
       addTrailingSlash: false,
+      cors: {
+        origin: process.env.NEXT_PUBLIC_URL,
+      },
+    });
+    // console.log(io);
+    io.on("connection", function (socket) {
+      socket.on("SOCKET_RECEIVED", ({ notification }) => {
+        io.emit(`SOCKET_RECEIVED`, notification);
+      });
     });
     res.socket.server.io = io;
   }
